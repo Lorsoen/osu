e602acdb088b49b5381626571b68e131623e0f3b
Lorenzo <fashim99@gmail.com>
2025-06-07 17:13:41 +0200

    CQ

--- osu.Game.Rulesets.Mania/UI/PlayfieldCoveringWrapper.cs
+++ osu.Game.Rulesets.Mania/UI/PlayfieldCoveringWrapper.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.ComponentModel;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.Color4Extensions;
--- osu.Game/Rulesets/Mods/ModBarrelRoll.cs
+++ osu.Game/Rulesets/Mods/ModBarrelRoll.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions;
-using osu.Framework.Graphics;
 using osu.Framework.Localisation;
 using osu.Game.Configuration;
 using osu.Game.Localisation;
